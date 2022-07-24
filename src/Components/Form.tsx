c54dc08e8aa86025cf564287301d1cca0b74083f
Lucy Ironmonger <37961811+lucy-ironmonger@users.noreply.github.com>
2022-07-24 21:07:54 +0100

    Setting up a form component

---
+++ src/Components/Form.tsx
@@ -0,0 +1,16 @@
+import {ReactElement} from "react";
+
+const Form = (): ReactElement => {
+    return (<form>
+        <div>
+            <label>Enter name</label>
+            <input/>
+        </div>
+        <div>
+            <label>Enter age</label>
+            <input/>
+        </div>
+    </form>)
+}
+
+export default Form;
